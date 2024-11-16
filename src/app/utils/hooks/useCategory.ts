ebc2e807d69189acd9c5eed74e8a534621d6ceba
mahmoudmagdy332 <mahmoudmagdy99925332@gmail.com>
2024-11-16 23:16:24 +0200

    all files

--- src/app/utils/hooks/useCategory.ts
+++
@@ -1,9 +0,0 @@
-import { useCategoryCoursesMutation } from "../../services/mutation";
-
-function useCategory() {
-  const { mutate, data, isPending, isSuccess, error, isError } =
-    useCategoryCoursesMutation();
-  return { mutate, isSuccess, isPending, isError, error, data };
-}
-
-export default useCategory;
